753fc667ac34b960e7e49887d485cef18a9a78c6
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-02 14:29:09 +0900

    Added index and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # List all tasks
+  get 'tasks', to: 'tasks#index'
+  # View details of each task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
