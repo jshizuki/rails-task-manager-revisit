a0b73a8ebc8bf57d2d644dd3503ccae503f44284
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-02 15:46:18 +0900

    Added form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # List all tasks
+  # As a user, I can list tasks
   get 'tasks', to: 'tasks#index'
-  # View details of each task
+  # As a user, I can add a new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # As a user, I can view the details of a task
   get 'tasks/:id', to: 'tasks#show', as: :task
 end
