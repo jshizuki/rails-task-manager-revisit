ce064fefdcb6e2f2c0bd0b1eed9d097d14b83840
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-03 21:08:49 +0900

    Coded update and destroy methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,31 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   # As a user, I can list tasks
   get 'tasks', to: 'tasks#index'
+
   # As a user, I can add a new task
   get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
   # As a user, I can view the details of a task
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # As a user, I can edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # still need to show the form
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # As a user, I can remove a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
