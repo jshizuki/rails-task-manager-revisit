c70eaf0ea429928d185fb6caecb58114a4733661
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-04 15:44:08 +0900

    Rewired to root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # As a user, I can list tasks
-  get 'tasks', to: 'tasks#index'
+  root to: 'tasks#index'
 
   # As a user, I can add a new task
   get 'tasks/new', to: 'tasks#new', as: :new_task
